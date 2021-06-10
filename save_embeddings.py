3ea4bdf70383674f5ccb8466e96708d7a478cf96
where-north <74485402+where-north@users.noreply.github.com>
2021-06-10 11:36:45 +0800

    Add files via upload

---
+++ save_embeddings.py
@@ -0,0 +1,33 @@
+"""
+Name : save_embeddings.py
+Author  : 北在哪
+Contect : 1439187192@qq.com
+Time    : 2021/4/2 17:32
+Desc:
+"""
+import torch
+from model.modeling_nezha import NeZhaModel
+from model.configuration_nezha import NeZhaConfig
+from transformers.models.bert.modeling_bert import BertModel
+from transformers.models.roberta.configuration_roberta import RobertaConfig
+
+base_config = NeZhaConfig.from_json_file('./nezha-cn-base/config.json')
+base_model = NeZhaModel.from_pretrained(
+    './nezha-cn-base/pytorch_model.bin', config=base_config)
+torch.save(base_model.embeddings.word_embeddings.state_dict(), 'embeddingNeZhaBase.pth')
+
+wwm_config = NeZhaConfig.from_json_file('./nezha-cn-wwm/bert_config.json')
+wwm_model = NeZhaModel.from_pretrained(
+    './nezha-cn-wwm/pytorch_model.bin', config=wwm_config)
+torch.save(wwm_model.embeddings.word_embeddings.state_dict(), 'embeddingNeZhaWWM.pth')
+
+roberta_config = RobertaConfig.from_json_file('./roberta-cn-wwm/bert_config.json')
+roberta_model = BertModel.from_pretrained(
+    './roberta-cn-wwm/pytorch_model.bin', config=roberta_config)
+torch.save(roberta_model.embeddings.word_embeddings.state_dict(), 'embeddingRoberta.pth')
+
+roberta_uer_config = RobertaConfig.from_json_file('./roberta-cn-uer/config.json')
+roberta_uer_model = BertModel.from_pretrained(
+    './roberta-cn-uer/pytorch_model.bin', config=roberta_uer_config)
+torch.save(roberta_uer_model.embeddings.word_embeddings.state_dict(), 'embeddingRobertaUer.pth')
+
